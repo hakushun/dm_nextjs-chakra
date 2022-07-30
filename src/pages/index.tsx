3ea745c63ebaeb8a2da9b85a23ce34f03f989a05
hakushun <hakushun.pianist@gmail.com>
2022-07-30 13:14:12 +0900

    refactor: Home pageにgetLayout接続

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,13 @@
-import type { NextPage } from "next";
+import type { ReactElement } from "react";
+import type { NextPageWithLayout } from "./_app";
+import { Layout } from "../components/Layout";
 
-const Home: NextPage = () => {
-  return <div>chakra</div>;
+const Home: NextPageWithLayout = () => {
+  return <div>Main</div>;
+};
+
+Home.getLayout = function getLayout(page: ReactElement) {
+  return <Layout>{page}</Layout>;
 };
 
 export default Home;
