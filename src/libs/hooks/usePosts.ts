f52f09424be68cca8f7296d4486d40c4310125da
hakushun <hakushun.pianist@gmail.com>
2022-07-30 13:53:41 +0900

    feat: usePost実装

---
+++ src/libs/hooks/usePosts.ts
@@ -0,0 +1,14 @@
+import useSWR from "swr";
+import { fetcher } from "../fetch/fetcher";
+
+export const usePosts = () => {
+  const { data } = useSWR<IPost[]>(
+    "https://jsonplaceholder.typicode.com/posts",
+    fetcher
+  );
+
+  return {
+    posts: data ?? [],
+    isLoading: !data,
+  };
+};
