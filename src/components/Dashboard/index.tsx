8f9d8409acf55df0353ebc6c65e2cba8a326f924
hakushun <hakushun.pianist@gmail.com>
2022-07-30 20:01:01 +0900

    refactor: DashboardにAlbumのtotal追加

--- src/components/Dashboard/index.tsx
+++ src/components/Dashboard/index.tsx
@@ -1,4 +1,5 @@
 import { Stack } from '@chakra-ui/react';
+import { useAlbums } from '../../libs/hooks/useAlbums';
 import { usePosts } from '../../libs/hooks/usePosts';
 import { useTodos } from '../../libs/hooks/useTodos';
 import { useUsers } from '../../libs/hooks/useUsers';
@@ -9,13 +10,15 @@ export const Dashboard = () => {
   const { posts, isLoading: postsLoading } = usePosts();
   const { todos, isLoading: todosLoading } = useTodos();
   const { users, isLoading: usersLoading } = useUsers();
+  const { albums, isLoading: albumsLoading } = useAlbums();
 
   return (
-    <ContentWrapper isLoading={postsLoading || todosLoading || usersLoading}>
+    <ContentWrapper isLoading={postsLoading || todosLoading || usersLoading || albumsLoading}>
       <Stack spacing={8} direction="row">
         <TotalCard title="Posts" total={posts.length} />
         <TotalCard title="Todos" total={todos.length} />
         <TotalCard title="Users" total={users.length} />
+        <TotalCard title="Albums" total={albums.length} />
       </Stack>
     </ContentWrapper>
   );
