0153c0dbd329876a445e7f9c716af8e28fcc88e0
hakushun <hakushun.pianist@gmail.com>
2022-07-30 20:00:45 +0900

    refactor: Navの内容を定数化

--- src/components/Nav/index.tsx
+++ src/components/Nav/index.tsx
@@ -1,37 +1,41 @@
 import { Link, List, ListItem } from '@chakra-ui/react';
 import NextLink from 'next/link';
 
+const MENU_LIST = [
+  {
+    path: '/',
+    title: 'Home',
+  },
+  {
+    path: '/posts',
+    title: 'Posts',
+  },
+  {
+    path: '/todos',
+    title: 'Todos',
+  },
+  {
+    path: '/users',
+    title: 'Users',
+  },
+  {
+    path: '/albums',
+    title: 'Albums',
+  },
+];
+
 export const Nav = () => {
   return (
     <List spacing="3">
-      <ListItem display="flex">
-        <NextLink href="/" passHref>
-          <Link flex={1} textAlign="center">
-            Home
-          </Link>
-        </NextLink>
-      </ListItem>
-      <ListItem display="flex">
-        <NextLink href="/posts" passHref>
-          <Link flex={1} textAlign="center">
-            Posts
-          </Link>
-        </NextLink>
-      </ListItem>
-      <ListItem display="flex">
-        <NextLink href="/todos" passHref>
-          <Link flex={1} textAlign="center">
-            Todos
-          </Link>
-        </NextLink>
-      </ListItem>
-      <ListItem display="flex">
-        <NextLink href="/users" passHref>
-          <Link flex={1} textAlign="center">
-            Users
-          </Link>
-        </NextLink>
-      </ListItem>
+      {MENU_LIST.map((menu) => (
+        <ListItem key={menu.path} display="flex">
+          <NextLink href={menu.path} passHref>
+            <Link flex={1} textAlign="center">
+              {menu.title}
+            </Link>
+          </NextLink>
+        </ListItem>
+      ))}
     </List>
   );
 };
