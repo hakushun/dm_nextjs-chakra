3c892c6399fa848e1e6c364a1c9039a615ba7210
hakushun <hakushun.pianist@gmail.com>
2022-07-30 19:32:20 +0900

    refactor: HeaderにAvatar表示

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,10 +1,13 @@
-import { Flex } from '@chakra-ui/react';
+import { Avatar, Button, Flex } from '@chakra-ui/react';
 import Image from 'next/image';
 
 export const Header = () => {
   return (
-    <Flex w="100%" maxW="1200px" alignItems="center" mx="auto">
+    <Flex w="100%" maxW="1200px" alignItems="center" justifyContent="space-between" mx="auto">
       <Image src="/200x50.png" alt="" width="200" height="50" />
+      <Button variant="ghost" p={0} borderRadius="50%">
+        <Avatar size="sm" />
+      </Button>
     </Flex>
   );
 };
