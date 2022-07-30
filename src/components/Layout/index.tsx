72c6c84ce81d7642dffe6887b60626913915b13c
hakushun <hakushun.pianist@gmail.com>
2022-07-30 13:12:59 +0900

    feat: Layoutコンポーネント作成

---
+++ src/components/Layout/index.tsx
@@ -0,0 +1,31 @@
+import { Grid, GridItem } from "@chakra-ui/react";
+import type { ReactNode } from "react";
+import { Header } from "../Header";
+import { Nav } from "../Nav";
+
+type Props = {
+  children: ReactNode;
+};
+
+export const Layout = ({ children }: Props) => {
+  return (
+    <Grid
+      templateAreas={`"header header"
+                      "nav main"`}
+      gridTemplateRows={"auto 1fr"}
+      gridTemplateColumns={"max(200px, 15%) 1fr"}
+      h="100vh"
+      color="blackAlpha.700"
+      fontWeight="bold">
+      <GridItem bg="orange.300" area={"header"} as="header">
+        <Header />
+      </GridItem>
+      <GridItem p="4" bg="pink.300" area={"nav"} as="nav">
+        <Nav />
+      </GridItem>
+      <GridItem bg="green.300" area={"main"} as="main">
+        {children}
+      </GridItem>
+    </Grid>
+  );
+};
